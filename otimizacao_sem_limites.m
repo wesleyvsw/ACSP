33fe14cd620f4a1a14f646269755f3d550136b4f
wesleyvsw <64928214+wesleyvsw@users.noreply.github.com>
2024-10-04 15:43:01 -0300

    Add files via upload

---
+++ otimizacao_sem_limites.m
@@ -0,0 +1,28 @@
+% Definindo as funções de custo
+%pkg load optim %Adicionando a biblioteca
+C1 = @(g1) 10 * g1^4 + 5;
+C2 = @(g2) 5 * g2^4 + 15;
+
+Aeq = [1, 1];
+beq = 100;
+
+%Função objetivo e limite das variaveis
+funcao_objetivo = @(x) C1(x(1)) + C2(x(2));
+lb = [0; 0];
+ub = [40;60];
+
+options = optimset('Display', 'iter'); % Exibe informações durante a otimização
+[x, fval] = fmincon(funcao_objetivo, [1; 99], [], [], Aeq, beq, lb, ub, [], options);
+
+%Resultados
+g1 = x(1);
+g2 = x(2);
+custo_total = fval;
+
+disp(['Gerador 1 (g1): ', num2str(g1)]);
+disp(['Gerador 2 (g2): ', num2str(g2)]);
+
+
+
+
+
